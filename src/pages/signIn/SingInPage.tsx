973a120b2a91d3fae8256d293e1da4a37ffe35b5
Yuchen Zhang <gary.zhang@lifebyte.io>
2021-11-14 17:20:41 +1100

    Add SignInForm UI

--- src/pages/signIn/SingInPage.tsx
+++ src/pages/signIn/SingInPage.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { AuthLayout } from "../../layouts/authLayout";
+import { SignInForm } from "./SignInForm";
 
 export const SignInPage: React.FC = (props) => {
-  return <AuthLayout></AuthLayout>;
+  return (
+    <AuthLayout>
+      <SignInForm />
+    </AuthLayout>
+  );
 };
